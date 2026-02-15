35d2007df34d20f4bf38368dc6f90b04ef1d2628
Numantia666 <auliarezki2434@gmail.com>
2026-02-15 18:41:36 +0700

    Add files via upload

---
+++ Praktikum 3/Latihan_5.py
@@ -0,0 +1,59 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        self.prev = None
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None 
+
+    def insert_at_end(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            new_node.prev = self.tail
+            self.tail = new_node
+    def display_forward(self):
+        print("\nSebelum dibalik:")
+        temp = self.head
+        while temp:
+            print(temp.data, end=" -> ")
+            temp = temp.next
+        print("null")
+    def display_backward(self):
+        print("\nSetelah dibalik:")
+        temp = self.tail
+        while temp:
+            print(temp.data, end=" -> ")
+            temp = temp.prev
+        print("null")
+
+
+    def delete_node(self,key):
+        temp = self.head
+        if temp and temp.data ==key:
+            self.head = temp.next
+            temp = None
+            return
+        prev = None
+        while temp and temp.data != key:
+            prev = temp
+            temp = temp.next
+        if temp is None:
+            return
+        prev.next = temp.next
+        temp = None
+
+    
+dll = DoublyLinkedList()
+for e in [1, 2, 3, 4, 5]:
+    dll.insert_at_end(e)
+
+dll.display_forward()
+dll.display_backward()
+dll.delete_node(13)
+
